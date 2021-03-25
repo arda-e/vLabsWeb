33eb5a37bee772383cd848bf6e978935df49e92c
aeren-netas <aeren@netas.com.tr>
2021-03-25 16:49:12 +0300

    product-hero-section build

--- vlabsweb/components/Products/ProductHeroSection.js
+++ vlabsweb/components/Products/ProductHeroSection.js
@@ -1,12 +1,20 @@
-const ProductHeroSection = () => {
+import React, {useState} from 'react'
+const ProductHeroSection = ({heading, subheading, backgroundSrc, textPosition}) => {
+    
     return (
+    textPosition === "center" ?
         <section className="flex flex-col items-center justify-center h-80 px-8 md:px-52">
             <div className="flex flex-col">
-                <div className="text-gray-600 text-4xl md:text-5xl font-semibold text-center">All About Test</div>
-                <div className="text-gray-600 text-md md:text-xl font-normal text-center mt-2">You’re wondering how you’ll quickly transition your team to remote work. People are stressed,
-                work feels scattered, projects are slipping, and it’s tough to see + manage everything.</div>
+                <div className="text-gray-600 text-4xl md:text-5xl font-semibold text-center">{heading}</div>
+                <div className="text-gray-600 text-md md:text-xl font-normal text-center mt-2">{subheading}</div>
             </div>
-        </section>
+        </section> : 
+            <section className="flex flex-col items-start justify-center h-80 px-8 md:px-52">
+            <div className="flex flex-col">
+                <div className="text-gray-600 text-4xl md:text-5xl font-semibold text-left">{heading}</div>
+                <div className="text-gray-600 text-md md:text-xl font-normal text-left mt-2">{subheading}</div>
+            </div>
+        </section> 
     )
 }
 
--- vlabsweb/pages/products/visiumfarm.js
+++ vlabsweb/pages/products/visiumfarm.js
@@ -6,7 +6,7 @@ const visiumfarm = () => {
     return (
         <section>
             <SecondaryMenu name="Farm"/>
-            <ProductHeroSection />
+            <ProductHeroSection heading="Lorem ipsum dolor sit amet" subheading="Lorem ipsum dolor sit amet" backgroundSrc="" textPosition="left"/>
         </section>
     )
 }
