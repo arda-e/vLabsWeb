3835d756387d0077f2e8472150b67b7bf67ae74f
aeren-netas <aeren@netas.com.tr>
2021-03-25 17:05:43 +0300

    testiminal section edit & add logo

--- vlabsweb/components/TestimonialBrands.js
+++ vlabsweb/components/TestimonialBrands.js
@@ -3,11 +3,11 @@ import Image from 'next/image'
 const TestimonialBrands = () => {
     return (
         <section className="flex justify-center bg-gray-50 py-10 flex-row px-10 space-x-4 md:space-x-16">
-            <div className="flex"><Image src="/visium-labs-logo.png" height="35" width="150" /></div>   
-            <div className="flex"><Image src="/visium-labs-logo.png" height="35" width="150" /></div>   
-            <div className="flex"><Image src="/visium-labs-logo.png" height="35" width="150" /></div>   
-            <div className="flex"><Image src="/visium-labs-logo.png" height="35" width="150" /></div>   
-            <div className="flex"><Image src="/visium-labs-logo.png" height="35" width="150" /></div>   
+            <div className="flex"><Image src="/img/aksigorta.png" height="40" width="150" /></div>   
+            <div className="flex"><Image src="/img/aktifbank.png" height="40" width="150" /></div>   
+            <div className="flex"><Image src="/img/belbim.png" height="40" width="150" /></div>   
+            <div className="flex"><Image src="/img/burgerking.png" height="40" width="150" /></div>   
+            <div className="flex"><Image src="/img/hyundai.png" height="40" width="150" /></div>   
         </section>
     )
 }
