0b22e245d2715ee6be4f518824cddf20083c7b4b
aeren-netas <aeren@netas.com.tr>
2021-03-26 15:36:59 +0300

    phosphor icon add

--- vlabsweb/pages/products/visiumfarm.js
+++ vlabsweb/pages/products/visiumfarm.js
@@ -3,6 +3,7 @@ import SecondaryMenu from "../../components/SecondaryMenu"
 import Head from 'next/head'
 import Text_Main from "../../components/Text/Text_Main"
 import H2 from "../../components/Text/H2"
+import { CloudArrowUp } from 'phosphor-react'
 
 const visiumfarm = () => {
     return (
@@ -20,8 +21,19 @@ const visiumfarm = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl potenti purus nisi, in nec et, ultricies. Vulputate convallis amet proin condimentum ut turpis neque nibh. Euismod consequat in massa tristique laoreet. Nisl consectetur diam amet adipiscing risus lectus tellus molestie dignissim. Sollicitudin quis malesuada lectus a, quam eget tellus.
             </Text_Main>
         </section>
-        <section>
-            
+        <section className="mt-10 mx-32">
+            <div className="flex">
+                <div className="flex flex-col">
+                    <div className="flex flex-row mb-3 items-center">
+                        <div className="flex mr-2 "><CloudArrowUp size={48} /></div>
+                        <div className="flex font-bold text-xl">Kendi lokasyonunuzda bir cihaz parkuruna sahip olmak hiç bu kadar kolay olmamıştı.
+
+</div>
+                    </div>
+                    <div>Text</div>
+                </div>
+            </div>
+            <div className="flex"></div>
         </section>
         </>
     )
