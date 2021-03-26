843b30df741f7e93e15683e8d78259d5f50d2603
aeren-netas <aeren@netas.com.tr>
2021-03-26 10:21:30 +0300

    about us layout and dummy text add

--- vlabsweb/pages/about-us/index.js
+++ vlabsweb/pages/about-us/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import SecondaryMenu from '../../components/SecondaryMenu'
+import ProductHeroSection from '../../components/Products/ProductHeroSection'
 
 const index = () => {
     return (
-        <div>
-            <SecondaryMenu />
+        <div className="max-w-8xl mx-auto">
+            <ProductHeroSection heading="About Us" textPosition="center" />
+            <div className="mt-10 font-base text-gray-600 leading-8 mx-72 ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
+Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
+            </div>
+            <div className="mt-4 font-base text-gray-600 leading-8 mx-72 ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
+Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
+            </div>
         </div>
     )
 }
