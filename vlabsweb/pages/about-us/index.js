5d074ed2f12db76374922d83962bf3a751016cdd
aeren-netas <aeren@netas.com.tr>
2021-03-29 09:02:48 +0300

    about us sayfa düzenlemesi

--- vlabsweb/pages/about-us/index.js
+++ vlabsweb/pages/about-us/index.js
@@ -1,20 +1,25 @@
 import ProductHeroSection from '../../components/Products/ProductHeroSection'
+import ResponsiveWrapper from '../../components/ResponsiveWrapper'
 
 const index = () => {
     return (
-        <div className="max-w-8xl mx-auto">
+        <>
             <ProductHeroSection heading="About Us" textPosition="center" />
-            <div className="mt-10 font-base text-gray-600 leading-8 mx-72 ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
-Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
-Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
+            <ResponsiveWrapper>
+                <div className="my-10">
+                    <div className="font-base text-gray-600 leading-8  ">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
+                        Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
             </div>
-            <div className="mt-4 font-base text-gray-600 leading-8 mx-72 ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
-Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
-Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
+                    <div className="mt-4 font-base text-gray-600 leading-8 ">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta.
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus.
+                        Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.
             </div>
-        </div>
+                </div>
+            </ResponsiveWrapper>
+        </>
     )
 }
 
