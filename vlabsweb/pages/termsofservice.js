f92c0b4f435e62290b61a708049b3183097aeb27
aeren-netas <aeren@netas.com.tr>
2021-03-29 09:02:24 +0300

    termosservice page init

---
+++ vlabsweb/pages/termsofservice.js
@@ -0,0 +1,16 @@
+import ProductHeroSection from '../components/Products/ProductHeroSection'
+import Text_Main from '../components/Text/Text_Main'
+const termsofservice = () => {
+    return (
+        <div>
+            <ProductHeroSection heading="Terms of Service" />
+            <section className="max-w-7xl container mx-auto px-8 sm:px-8 md:px-10 lg:px:24 xl:px-32 my-10">
+                <Text_Main>Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada.
+Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla quis lorem ut libero malesuada feugiat.</Text_Main>
+            </section>
+        </div>
+    )
+}
+
+export default termsofservice
